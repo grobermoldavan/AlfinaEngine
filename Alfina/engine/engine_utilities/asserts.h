b9be890702bf8da1e8846deee17233c0c8a43815
Ilya Baryshnikov <grober.moldavan@gmail.com>
2020-08-19 22:26:02 +0300

    Update asserts
    Added support for multiple assert output values

--- Alfina/engine/engine_utilities/asserts.h
+++ Alfina/engine/engine_utilities/asserts.h
@@ -3,14 +3,14 @@
 
 #include "logging\logging.h"
 
-#define AL_ASSERT_MSG_NO_DISCARD(condition, msg)	if (!(condition))	AL_LOG_NO_DISCARD(al::engine::Logger::Type::ERROR_MSG, msg)
+#define AL_ASSERT_MSG_NO_DISCARD(condition, ...)	if (!(condition))	AL_LOG_NO_DISCARD(al::engine::Logger::Type::ERROR_MSG, __VA_ARGS__)
 
 #if defined(AL_DEBUG)
-#	define AL_ASSERT_MSG(condition, msg)	if (!(condition))	AL_LOG(al::engine::Logger::Type::ERROR_MSG, msg)
+#	define AL_ASSERT_MSG(condition, ...)	if (!(condition))	AL_LOG(al::engine::Logger::Type::ERROR_MSG, __VA_ARGS__)
 #	define AL_ASSERT(condition)				if (!(condition))	AL_LOG(al::engine::Logger::Type::ERROR_MSG, "Assertion failed")
 #	define AL_NO_IMPLEMENTATION_ASSERT							AL_LOG(al::engine::Logger::Type::ERROR_MSG, "No method implementation provided")
 #else
-#	define AL_ASSERT_MSG(condition, msg)
+#	define AL_ASSERT_MSG(condition, ...)
 #	define AL_ASSERT(condition)
 #	define AL_NO_IMPLEMENTATION_ASSERT
 #endif
